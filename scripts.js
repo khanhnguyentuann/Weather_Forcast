fef5a10ed2d8a9662fcd0c0af9646b5d6583dff8
khanhnguyentuann <khanh.nguyentuann@gmail.com>
2023-04-29 00:02:20 +0700

    Update frontend and remove some function never use

--- scripts.js
+++ scripts.js
@@ -105,28 +105,3 @@ function updateWeatherData() {
 }
 // Update the weather data when the page loads
 updateWeatherData();
-
-document.getElementById('timeSlider').addEventListener('input', function (event) {
-  // Update the map based on the selected time
-  console.log('Selected time:', event.target.value);
-  // Update the weather data
-  updateWeatherData();
-});
-
-document.querySelectorAll('input[name="timeUnit"]').forEach(function (radio) {
-  radio.addEventListener('change', function (event) {
-    // Update the map based on the selected time unit (hours or days)
-    console.log('Selected time unit:', event.target.value);
-    // Update the weather data
-    updateWeatherData();
-  });
-});
-
-document.querySelectorAll('aside button').forEach(function (button) {
-  button.addEventListener('click', function (event) {
-    // Update the map based on the selected weather parameter
-    console.log('Selected weather parameter:', event.target.textContent);
-    // Update the weather data
-    updateWeatherData();
-  });
-});
