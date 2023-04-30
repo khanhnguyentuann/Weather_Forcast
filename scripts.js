05cb150db5821adf3ec742c2dbc636e64d2dcd3a
khanhnguyentuann <khanh.nguyentuann@gmail.com>
2023-04-30 18:36:32 +0700

    reformat the scripts.js file and some code tweaks

--- scripts.js
+++ scripts.js
@@ -1,130 +1,47 @@
-var map;
-var rainLayer;
-var windLayer;
-var tempLayer;
-var cloudsLayer;
-var waveLayer;
-var airLayer;
+var map, rainLayer, windLayer, tempLayer, cloudsLayer, airLayer;
+var api_key = 'c9c8e558cd1dad0583f2600da8c72b7e';
 
 function initMap() {
-    map = L.map('map', {
-        zoomControl: false // Tắt nút phóng to/thu nhỏ mặc định
-    }).setView([10.762622, 106.660172], 13); // Tọa độ trung tâm của Thành phố Hồ Chí Minh và mức phóng đại 13
+    map = L.map('map', {zoomControl: false}).setView([10.762622, 106.660172], 13);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        maxZoom: 19
-    }).addTo(map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19}).addTo(map);
 
-    // Tạo nút phóng to/thu nhỏ mới và đặt bên phải
-    var zoomControl = L.control.zoom({
-        position: 'topright'
-    }).addTo(map);
+    L.control.zoom({position: 'topright'}).addTo(map);
 
     map.on('click', onMapClick);
 
-    // Khởi tạo rain layer với opacity 0.5
-    rainLayer = L.tileLayer('https://{s}.tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=fed32be58df3a0bd09ff4c02c0da7443', {
-        opacity: 1.0
-    });
+    addLayerToggle("rain", rainLayer);
+    addLayerToggle("wind", windLayer);
+    addLayerToggle("temp", tempLayer);
+    addLayerToggle("clouds", cloudsLayer);
+    addLayerToggle("air", airLayer);
+    // addLayerToggle("humidity", humidityLayer);
+}
 
-    windLayer = L.tileLayer(`https://{s}.tile.openweathermap.org/map/wind_new/{z}/{x}/{y}.png?appid=fed32be58df3a0bd09ff4c02c0da7443`, {
-        opacity: 1.0
-    });
-
-    tempLayer = L.tileLayer(`https://{s}.tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=fed32be58df3a0bd09ff4c02c0da7443`, {
-        opacity: 1.0
-    });
-
-    cloudsLayer = L.tileLayer(`https://{s}.tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=fed32be58df3a0bd09ff4c02c0da7443`, {
-        opacity: 1.0
-    });
-
-    waveLayer = L.tileLayer(`https://{s}.tile.openweathermap.org/map/sea/{z}/{x}/{y}.png?appid=fed32be58df3a0bd09ff4c02c0da7443`, {
-        opacity: 1.0
-    });
-
-    airLayer = L.tileLayer(`https://{s}.tile.openweathermap.org/map/air_pollution/{z}/{x}/{y}.png?appid=fed32be58df3a0bd09ff4c02c0da7443`, {
-        opacity: 1.0
-    });
-
-    document.getElementById("rain").addEventListener("click", function () {
-        if (map.hasLayer(rainLayer)) {
-            map.removeLayer(rainLayer);
-        } else {
-            rainLayer.addTo(map);
-        }
-    });
-
-    document.getElementById("wind").addEventListener("click", function () {
-        if (map.hasLayer(windLayer)) {
-            map.removeLayer(windLayer);
-        } else {
-            windLayer.addTo(map);
-        }
-    });
-
-    document.getElementById("temp").addEventListener("click", function () {
-        if (map.hasLayer(tempLayer)) {
-            map.removeLayer(tempLayer);
-        } else {
-            tempLayer.addTo(map);
-        }
-    });
-
-    document.getElementById("clouds").addEventListener("click", function () {
-        if (map.hasLayer(cloudsLayer)) {
-            map.removeLayer(cloudsLayer);
-        } else {
-            cloudsLayer.addTo(map);
-        }
-    });
-
-    document.getElementById("wave").addEventListener("click", function () {
-        if (map.hasLayer(waveLayer)) {
-            map.removeLayer(waveLayer);
-        } else {
-            waveLayer.addTo(map);
-        }
-    });
-
-    document.getElementById("air").addEventListener("click", function () {
-        if (map.hasLayer(airLayer)) {
-            map.removeLayer(airLayer);
-        } else {
-            airLayer.addTo(map);
-        }
+function addLayerToggle(id, layer) {
+    document.getElementById(id).addEventListener("click", function () {
+        toggleLayer(layer);
     });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    initMap();
-});
-
-var shareModal = document.getElementById("share-modal");
-var shareIcon = document.querySelector(".share-icon");
-var closeBtn = document.querySelector(".close");
-
-shareIcon.addEventListener("click", function () {
-    shareModal.style.display = "block";
-});
-
-closeBtn.addEventListener("click", function () {
-    shareModal.style.display = "none";
-});
-
-window.addEventListener("click", function (event) {
-    if (event.target == shareModal) {
-        shareModal.style.display = "none";
+function toggleLayer(layer) {
+    if (map.hasLayer(layer)) {
+        map.removeLayer(layer);
+    } else {
+        layer.addTo(map);
     }
-});
+}
+
+function createTileLayer(urlTemplate) {
+    return L.tileLayer(urlTemplate, {
+        opacity: 1.0,
+    });
+}
 
 function onMapClick(e) {
-    var api_key = 'fed32be58df3a0bd09ff4c02c0da7443';
     var lat = e.latlng.lat;
     var lon = e.latlng.lng;
 
-    // Lấy thông tin thời tiết từ OpenWeatherMap
     var url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`;
 
     fetch(url)
@@ -134,7 +51,6 @@ function onMapClick(e) {
             var rain_1h = data.rain && data.rain['1h'] ? data.rain['1h'] + " mm" : "N/A";
             var temperature = data.main && data.main.temp ? data.main.temp + " °C" : "N/A";
             var clouds = data.clouds && data.clouds.all ? data.clouds.all + " %" : "N/A";
-            var wave_height = data.waves && data.waves.height ? data.waves.height + " m" : "N/A";
             var air_pressure = data.main && data.main.pressure ? data.main.pressure + " hPa" : "N/A";
             var humidity = data.main && data.main.humidity ? data.main.humidity + " %" : "N/A";
 
@@ -142,10 +58,9 @@ function onMapClick(e) {
           <div>
             <h3>Thời tiết tại ${data.name}</h3>
             <p>Tốc độ gió: ${wind_speed}</p>
-            <p>Lượng mưa: ${rain_1h}</p>
+            <p>Lượng mưa 1h: ${rain_1h}</p>
             <p>Nhiệt độ: ${temperature}</p>
-            <p>Mây: ${clouds}</p>
-            <p>Chiều cao sóng: ${wave_height}</p>
+            <p>Mây che phủ: ${clouds}</p>
             <p>Áp suất không khí: ${air_pressure}</p>
             <p>Độ ẩm: ${humidity}</p>
           </div>
@@ -160,3 +75,38 @@ function onMapClick(e) {
             console.error('Error fetching weather data:', error);
         });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    initMap();
+});
+
+function setupShareModal() {
+    var shareModal = document.getElementById("share-modal");
+    var shareIcon = document.querySelector(".share-icon");
+    var closeBtn = document.querySelector(".close");
+
+    shareIcon.addEventListener("click", function () {
+        shareModal.style.display = "block";
+    });
+
+    closeBtn.addEventListener("click", function () {
+        shareModal.style.display = "none";
+    });
+
+    window.addEventListener("click", function (event) {
+        if (event.target == shareModal) {
+            shareModal.style.display = "none";
+        }
+    });
+}
+
+setupShareModal();
+
+// Initialize layers with the appropriate URLs
+rainLayer = createTileLayer(`https://{s}.tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=${api_key}`);
+windLayer = createTileLayer(`https://{s}.tile.openweathermap.org/map/wind_new/{z}/{x}/{y}.png?appid=${api_key}`);
+tempLayer = createTileLayer(`https://{s}.tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=${api_key}`);
+cloudsLayer = createTileLayer(`https://{s}.tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=${api_key}`);
+airLayer = createTileLayer(`https://{s}.tile.openweathermap.org/map/pressure_new/{z}/{x}/{y}.png?appid=${api_key}`);
+// humiditylayer cần dùng phải trả phí
+
