e23136a3e95da61a5b043bacd31e6d1d290737ec
khanhnguyentuann <khanh.nguyentuann@gmail.com>
2023-05-01 22:28:46 +0700

    edit some interface details

--- login_register.js
+++ login_register.js
@@ -1,10 +1,9 @@
 function initMap() {
     var map = L.map('map', { zoomControl: false }).setView([14.0583, 108.2772], 5);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        maxZoom: 18
-    }).addTo(map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',maxZoom: 18}).addTo(map);
+
+    L.control.zoom({ position: 'topright' }).addTo(map);
 }
 
 document.addEventListener('DOMContentLoaded', initMap);
