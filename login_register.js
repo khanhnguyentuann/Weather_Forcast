bc827aad86481f0528b2d2f18777e7e16d9a1940
khanhnguyentuann <khanh.nguyentuann@gmail.com>
2023-05-02 21:54:41 +0700

    login fail fb

--- login_register.js
+++ login_register.js
@@ -68,6 +68,47 @@ $(document).ready(function () {
         });
     });
 
+    $('.facebook-login').click(function () {
+        FB.login(
+            function (response) {
+                if (response.authResponse) {
+                    // Người dùng đã đăng nhập thành công bằng Facebook
+                    // Bạn có thể lấy thông tin người dùng và xử lý tiếp theo
+                    FB.api('/me', function (response) {
+                        console.log('Good to see you, ' + response.name + '.');
+                    });
+                } else {
+                    console.log('User cancelled login or did not fully authorize.');
+                }
+            },
+            { scope: 'public_profile,email' }
+        );
+    });
+
+    window.fbAsyncInit = function () {
+        FB.init({
+            appId: '944575006963713',
+            cookie: true,
+            xfbml: true,
+            version: 'v12.0',
+        });
+    
+        FB.AppEvents.logPageView();
+    };
+    
+    (function (d, s, id) {
+        var js,
+            fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
+    
+
     $('#register-link').click(function () {
         window.location.href = 'register.html';
     });
